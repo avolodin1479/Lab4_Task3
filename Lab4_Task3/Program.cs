f15e71e6df74b4f4e5366195324e54aa0b0c9cd5
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-06 22:50:36 +0600

    Добавьте файлы проекта.

---
+++ Lab4_Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительные числа A, B и C");
+            float A = Convert.ToInt32(Console.ReadLine());
+            float B = Convert.ToInt32(Console.ReadLine());
+            float C = Convert.ToInt32(Console.ReadLine());
+            int N = 0;
+            float X;
+            while ((A-C) >=0)
+            {
+                A -= C;
+                X = B;
+                while (X-C >=0)
+                {
+                    X -= C;
+                    ++N;
+                }
+                
+            }
+
+            Console.WriteLine("Количество квадратов = {0}",N);
+            Console.ReadKey();
+        }
+    }
+}
